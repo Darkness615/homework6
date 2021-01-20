6806fad91293a13c6ceb9ad733676458e2dcbb12
Darkness615 <76398142+Darkness615@users.noreply.github.com>
2021-01-20 18:51:40 +0300

    Update Animal.java

--- src/animal/lesson6/Animal.java
+++ src/animal/lesson6/Animal.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Constructor;
 
 
 public abstract class Animal {
-    public static int count;
+    private static int count;
     private int MaxRundistance;
     private int MaxSwimdistance;
 
